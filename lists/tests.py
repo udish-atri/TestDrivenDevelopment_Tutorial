0b5d0a90e6d4f93f5c895dfdb0af99825eb0cc23
udish-atri <udishkum@atrihub.io>
2025-11-06 11:11:53 -0800

    refactor(chapter5-tdd-training): added new testcase
    [ICTRAINING-2242]

--- lists/tests.py
+++ lists/tests.py
@@ -2,10 +2,16 @@ from django.test import TestCase
 
 
 class HomePageTest(TestCase):
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
 
-    def test_renders_home_page_content(self):
+    def test_renders_input_form(self):
         response = self.client.get("/")
-        self.assertContains(response, "To-Do")
+        self.assertContains(response, '<form method="POST">')
+        self.assertContains(response, '<input name="item_text"')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertContains(response, "A new list item")
+        self.assertTemplateUsed(response, "home.html")
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,11 @@
-# from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home_page(request):
-    # return HttpResponse("<html><title>To-Do lists</title></html>")
-    return render(request, "home.html")
+    # if request.method == "POST":
+    #     return HttpResponse("You submitted: " + request.POST["item_text"])
+    # # return HttpResponse("<html><title>To-Do lists</title></html>")
+    # return render(request, "home.html")
+    return render(
+        request, "home.html", {"new_item_text": request.POST.get("item_text", "")}
+    )
