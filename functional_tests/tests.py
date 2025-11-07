f2fa4c3d8ac4285569b268a8e209bd975c823c1f
udish-atri <udishkum@atrihub.io>
2025-11-07 10:29:27 -0800

    feat(chapter6-tdd-training): make functional_tests an app, use LiveServerTestCase
    functional tests have been isolated and transformed into an app instead. The strategy is to run tests using liveservertestcase class.
    
    [ICTRAINING-2243]

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,11 @@
-# from email import header
-import unittest
 import time
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -21,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_todo_list(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
@@ -54,7 +53,3 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # Satisfied, she goes back to sleep
-
-
-if __name__ == "__main__":
-    unittest.main()
