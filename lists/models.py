6ee56e0c02194cd77980837587c05d432c85cd42
udish-atri <udishkum@atrihub.io>
2025-11-07 14:21:27 -0800

    feat(chapter7-tdd-training): implement multiple lists with unique URLs
    [ICTRAINING-2244]

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class List(models.Model):
+    pass
+
+
 # Create your models here.
 class Item(models.Model):
     text = models.TextField(default="")
+    list = models.ForeignKey(
+        List, on_delete=models.CASCADE, default=None, null=True, blank=True
+    )
--- superlists/urls.py
+++ superlists/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from lists.views import home_page
+from lists import views
 from django.urls import path
 
 urlpatterns = [
-    path("", home_page, name="home"),
+    path("", views.home_page, name="home"),
+    path("lists/new", views.new_list, name="new_list"),
+    path("lists/<int:list_id>/", views.view_list, name="view_list"),
+    path("lists/<int:list_id>/add_item", views.add_item, name="add_item"),
 ]
